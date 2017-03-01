dfd27bc6ee212db25d45f49b33d3f2ef90440388
Alex Lockhart <lockhart@Alexs-MacBook-Pro.local>
2017-03-01 17:59:03 -0400

    fix port for app and make it listen

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@
 const express = require('express');
 const app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function(req, res) {
     res.status(200).send('CrapMap Server is Running');
+});
+
+app.listen(app.get('port'), function() {
+	console.log(`Listening on port ${app.get('port')}!`);
 });
