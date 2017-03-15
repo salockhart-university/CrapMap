199a0a4c370f186c200c50020bc9f4a5e5e96de7
Geoffrey Caven <geoffrey@bravenew.world>
2017-03-14 21:40:40 -0300

    Started on the Map activity, still trying to figure out how to make the googleAPIclient stuff work with the api 23 permission requests

---
+++ app/app/src/main/java/ca/team2/crapmap/MapsActivity.java
@@ -0,0 +1,198 @@
+package ca.team2.crapmap;
+
+import android.app.admin.SecurityLog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.Manifest;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    private LocationRequest pollingLocationRequest;
+    private GoogleApiClient googleApiClient;
+
+    private LatLng currentLocation;
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+    private Marker currentLocationMarker;
+
+    private final int LOCATION_PERMISSION_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        if (checkLocationPermission()) {
+            onPermissionGranted();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    /*
+     * Executed when the app gains location access permissions
+     */
+    public void onPermissionGranted() {
+        try {
+            Log.i("onPermissionGranted","start the fancy stuff");
+            mMap.setMyLocationEnabled(true);
+            googleApiClient.connect();
+        } catch (SecurityException e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        checkLocationPermission();
+    }
+
+    /*
+     * Executed when googleApiClient successfully connects to the API
+     */
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i("googleApiClient","onConnected executing");
+        if (checkLocationPermission()) {
+            Location lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (lastKnownLocation != null) {
+                currentLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
+            }
+            pollingLocationRequest = new LocationRequest();
+            pollingLocationRequest.setInterval(5000);
+            pollingLocationRequest.setFastestInterval(3000);
+            pollingLocationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, pollingLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //do something?
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        //show an error probably
+    }
+
+    @Override
+    public void onLocationChanged(Location newLocation) {
+        mMap.clear();
+        if (currentLocationMarker != null) {
+            currentLocationMarker.remove();
+        }
+        currentLocation = new LatLng(newLocation.getLatitude(), newLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+    }
+
+    private boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.permission_title_location))
+                        .setMessage(getString(R.string.permission_explanation_location))
+                        .setPositiveButton(R.string.permission_confirmation, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        LOCATION_PERMISSION_REQUEST);
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_map)
+                        .show();
+                return false;
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST);
+                return false;
+
+            }
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i("permission","GRANTED");
+                    onPermissionGranted();
+
+                } else {
+                    // permission denied, boo!
+                    Log.i("permission","DENIED");
+                    // throw new SecurityException("Don't deny my permissions, bro!!");
+                }
+                return;
+            }
+        }
+    }
+}
