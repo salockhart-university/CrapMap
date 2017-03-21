06696c042a07741d21ed5b0335195906c9bfa911
Brandon Min <br239881@gmail.com>
2017-03-21 13:25:17 -0300

    init commit for comment activity

--- app/app/src/main/java/ca/team2/crapmap/NewCommentActivity.java
+++ app/app/src/main/java/ca/team2/crapmap/NewCommentActivity.java
@@ -1,8 +1,13 @@
 package ca.team2.crapmap;
 
-/**
- * Created by bmin on 2017-03-17.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class NewCommentActivity {
+public class NewCommentActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_comment);
+    }
+
 }
