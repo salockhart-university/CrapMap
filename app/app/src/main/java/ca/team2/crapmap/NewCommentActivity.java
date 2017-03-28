d43278b8fb8faf3a715154df9236512030a87030
Brandon Min <br239881@gmail.com>
2017-03-28 00:00:25 -0300

    Totally not tested codes for new comment/review activity

--- app/app/src/main/java/ca/team2/crapmap/NewCommentActivity.java
+++ app/app/src/main/java/ca/team2/crapmap/NewCommentActivity.java
@@ -1,13 +1,64 @@
 package ca.team2.crapmap;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.EditText;
 
 public class NewCommentActivity extends AppCompatActivity {
+
+    private RatingBar cleanliness;
+    private RatingBar accessibility;
+    private RatingBar availability;
+    private EditText comment;
+    private ImageButton save;
+
+    private String baseApiUrl;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_comment);
+
+        cleanliness = (RatingBar) findViewById(R.id.rbCleanliness);
+        accessibility = (RatingBar) findViewById(R.id.rbAccessibility);
+        availability = (RatingBar) findViewById(R.id.rbAccessibility);
+        comment = (EditText) findViewById(R.id.tbComment);
+        save = (ImageButton) findViewById(R.id.btSave);
+
+        baseApiUrl = getIntent().getStringExtra("baseApiUrl");
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewComment();
+            }
+        });
+    }
+
+    public void createNewComment() {
+        float clean = cleanliness.getRating();
+        float avail = availability.getRating();
+        float access = accessibility.getRating();
+        String commentString = comment.getText().toString();
+
+        PostNewComment postNewComment = new PostNewComment(baseApiUrl, clean, avail, access, commentString);
+        String response = null;
+        try{
+            response = (String) postNewComment.execute().get();
+        }
+        catch(Exception e){}
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("response", response);
+        //TODO: send bad response if request failed
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+
     }
 
 }
---
+++ app/app/src/main/java/ca/team2/crapmap/PostNewComment.java
@@ -0,0 +1,115 @@
+package ca.team2.crapmap;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PostNewComment extends AsyncTask {
+    private float cleanliness, accessibility, availability;
+    private String comment, stringUrl;
+    String bathroomId;
+
+    //TODO: add user and bathroom parameters
+    public PostNewComment(String stringUrl, float cleanliness, float accessibility, float availability, String comment) {
+        this.stringUrl = stringUrl;
+        this.cleanliness = cleanliness;
+        this.accessibility = accessibility;
+        this.availability = availability;
+        this.comment = comment;
+    }
+
+    public float getCleanliness() {
+        return cleanliness;
+    }
+
+    public void setCleanliness(float cleanliness) {
+        this.cleanliness = cleanliness;
+    }
+
+    public float getAccessibility() {
+        return accessibility;
+    }
+
+    public void setAccessibility(float accessibility) {
+        this.accessibility = accessibility;
+    }
+
+    public float getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(float availability) {
+        this.availability = availability;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getStringUrl() {
+        return stringUrl;
+    }
+
+    public void setStringUrl(String stringUrl) {
+        this.stringUrl = stringUrl;
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        //TODO: add a user and check for bathroom
+        try{
+            URL url = new URL(this.stringUrl);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.connect();
+
+            JSONObject body = new JSONObject();
+            JSONObject stars = new JSONObject();
+
+            stars.put("cleanliness", cleanliness);
+            stars.put("availability", availability);
+            stars.put("accessibility", accessibility);
+
+            body.put("stars", stars);
+            body.put("review", comment);
+
+            DataOutputStream printout = new DataOutputStream(connection.getOutputStream());
+            printout.writeBytes(body.toString());
+            printout.flush();
+            printout.close();
+
+            InputStream is = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            JSONObject json = new JSONObject(sb.toString());
+
+            return json.toString();
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+}
