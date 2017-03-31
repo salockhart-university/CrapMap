841ca023f04b07b25b26733485f524c69d9266fe
salockhart <salexlockhart@gmail.com>
2017-03-31 13:38:07 -0300

    fix my shit

--- app/app/src/main/java/ca/team2/crapmap/RetrieveBathrooms.java
+++ app/app/src/main/java/ca/team2/crapmap/RetrieveBathrooms.java
@@ -14,7 +14,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-import ca.team2.crapmap.model.Bathroom;
+import ca.team2.crapmap.Bathroom;
 
 /**
  * Created by geoffreycaven on 2017-03-15.
